dac93e1e1c018b493f60192f32de99027a6c556e
abelimachi <limachiabel54@gmail.com>
2023-09-19 22:14:42 -0300

    actualizacion de repositorio

--- scripts/script.js
+++ scripts/script.js
@@ -6,22 +6,22 @@ addEventListener("load", () => {
     const btnJuanCartasegna = document.getElementById("btn-juan-cartasegna")
 
     btnArielIbarbalz.addEventListener("click", () => {
-        location.href = "/pages/ariel-ibarbalz.html"
+        location.href = "./pages/ariel-ibarbalz.html"
     })
 
     btnJuanPaez.addEventListener("click", () => {
-        location.href = "/pages/juan-paez.html"
+        location.href = "./pages/juan-paez.html"
     })
 
     btnDanielFrias.addEventListener("click", () => {
-        location.href = "/pages/daniel-frias.html"
+        location.href = "./pages/daniel-frias.html"
     })
 
     btnAbelLimachi.addEventListener("click", () => {
-        location.href = "/pages/abel-limachi.html"
+        location.href = "./pages/abel-limachi.html"
     })
 
     btnJuanCartasegna.addEventListener("click", () => {
-        location.href = "/pages/juan-cartasegna.html"
+        location.href = "./pages/juan-cartasegna.html"
     })
 })
